8103ebb257520e1ade8d65bd48483244e951f857
Shalev <shalevaharon125@gmail.com>
2023-09-26 12:18:06 +0300

    Updated version

--- tests/devices_test_cases.py
+++ tests/devices_test_cases.py
@@ -5,15 +5,37 @@ from imports.imports import *
 class Devices(TestCase):
 
     def setUp(self) -> None:
-        self.my_driver = Driver()
-        self.my_driver.driver.get(link)
+        self.driver_object = Driver()
+        self.my_driver = self.driver_object.driver
+        self.my_driver.get(link)
+
+    @classmethod
+    def setUpClass(cls):
+        # Navigate to the folder and run 'npm start' in the background
+        cls.server_process = subprocess.Popen(
+            "cd C:/Users/shale/sources/Plasma-Dashboard && npm start",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        # Give the server some time to start
+        time.sleep(5)
+
     # --------------------------------------------------------------------------------------------------------
 
-
-
-
+    # [Devices]
+    def test_click_menu_devices(self):
+        Actions(self.my_driver).click_element('XPATH', menu_devices)
+        Actions(self.my_driver).url_change()
+        self.assertTrue(self.my_driver.current_url == 'http://localhost/devices')
 
     # --------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def tearDownClass(cls):
+        # Terminate the server process
+        cls.server_process.terminate()
+
     def tearDown(self) -> None:
         time.sleep(1)
-        self.my_driver.driver.quit()
+        self.my_driver.quit()
--- tests/mapping_test_cases.py
+++ tests/mapping_test_cases.py
@@ -5,15 +5,39 @@ from imports.imports import *
 class Mapping(TestCase):
 
     def setUp(self) -> None:
-        self.my_driver = Driver()
-        self.my_driver.driver.get(link)
+        self.driver_object = Driver()
+        self.my_driver = self.driver_object.driver
+        self.my_driver.get(link)
+
+    @classmethod
+    def setUpClass(cls):
+        # Navigate to the folder and run 'npm start' in the background
+        cls.server_process = subprocess.Popen(
+            "cd C:/Users/shale/sources/Plasma-Dashboard && npm start",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        # Give the server some time to start
+        time.sleep(5)
+
     # --------------------------------------------------------------------------------------------------------
 
-
+    # [Mapping]
+    def test_click_menu_mapping(self):
+        Actions(self.my_driver).click_element('XPATH', menu_mapping)
+        Actions(self.my_driver).url_change()
+        self.assertTrue(self.my_driver.current_url == 'http://localhost/mapping')
 
 
 
     # --------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def tearDownClass(cls):
+        # Terminate the server process
+        cls.server_process.terminate()
+
     def tearDown(self) -> None:
         time.sleep(1)
-        self.my_driver.driver.quit()
+        self.my_driver.quit()
